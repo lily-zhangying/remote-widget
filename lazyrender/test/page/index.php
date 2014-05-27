cf17f9b29decf8ebb7ef75c060a2d275788348ea
lily-zhangying <zhangying3210@gmail.com>
2014-05-27 14:10:36 +0800

    更新测试数据

--- lazyrender/test/page/index.php
+++ lazyrender/test/page/index.php
@@ -3,5 +3,10 @@
         "b" => array(
             "index" => "b_index",
             "value" => "b_value"
+        ),
+        "test" =>array(
+            "v" => array(
+                "value" => "test_value"
+            )
         )
     );
